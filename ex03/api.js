5e2d4896d5febdc6f6769d092701191bd6623340
gaoxinxiao <18210833386@163.com>
2021-01-28 20:43:58 -0800

    1

--- ex03/api.js
+++ ex03/api.js
@@ -6,7 +6,7 @@ http.createServer((req, res) => {
     const { method, url } = req
 
     console.log('method', method)
-    console.log('cookie',req.headers.cookie)
+    console.log('cookie', req.headers.cookie)
     if (method === 'GET' && url === '/') {
         fs.readFile('./index.html', (err, data) => {
             if (err) throw err
@@ -35,6 +35,22 @@ http.createServer((req, res) => {
         res.end(JSON.stringify({
             name: 'jerry'
         }))
+    } else if (method === 'POST' && url === '/api/save') {
+        let reqData = []
+        let size = 0
+        req.on('data', data => {
+            //开始读流
+            console.log('>>>req on', data)
+            reqData.push(data)
+            size += data.length
+        })
+        req.on('end', function () {
+            //读流完成
+            console.log('end')
+            const data = Buffer.concat(reqData, size)
+            console.log('data', size, data.toString())
+            res.end(`formdata:${data.toString()}`)
+        })
     }
 
 }).listen(4000, () => console.log('api http://localhost:4000'))
